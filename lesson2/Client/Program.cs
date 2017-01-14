7e0e86530eec42bc13a9817bf4497e3d0c1da465
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-01-14 09:50:11 +0200

    Client/Server without configuration

---
+++ lesson2/Client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    [ServiceContract]
+    interface IHello
+    {
+        [OperationContract]
+        string sayHello(string name);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+
+            // TCP
+            ChannelFactory<IHello> cfNet = new ChannelFactory<IHello>(
+                new NetTcpBinding(),
+                new EndpointAddress(@"net.tcp://localhost:3121/Hello")
+            );
+
+            IHello HelloTcp = cfNet.CreateChannel();
+            string TcpResult = HelloTcp.sayHello("TCP");
+
+            Console.WriteLine(TcpResult);
+
+            // PIPE
+            ChannelFactory<IHello> cfPipe = new ChannelFactory<IHello>(
+                new NetNamedPipeBinding(),
+                new EndpointAddress(@"net.pipe://localhost/Hello")
+            );
+
+            IHello HelloPipe = cfPipe.CreateChannel();
+            string PipeResult = HelloPipe.sayHello("PIPE");
+
+            Console.WriteLine(PipeResult);
+        }
+    }
+}
---
+++ lesson2/Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Server
+{
+    [ServiceContract]
+    interface IHello
+    {
+        [OperationContract]
+        string sayHello(string name);
+    }
+
+    public class Hello : IHello
+    {
+        public string sayHello(string name)
+        {
+            Console.WriteLine("Hello, {0}", name);
+            Console.WriteLine(OperationContext.Current.RequestContext.RequestMessage.ToString());
+
+            return name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(
+                typeof(Hello),
+                new Uri[]
+                {
+                    new Uri(@"net.tcp://localhost:3121/Hello"),
+                    new Uri(@"net.pipe://localhost/Hello")
+                }
+            );
+
+            host.AddServiceEndpoint(typeof(IHello), new NetTcpBinding(), "net.tcp://localhost:3121/Hello");
+            host.AddServiceEndpoint(typeof(IHello), new NetNamedPipeBinding(), "net.pipe://localhost/Hello");
+
+            host.Open();
+        }
+    }
+}
